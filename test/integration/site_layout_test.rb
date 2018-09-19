6df0f861d431e4edc5b38d93c44b5feb51825280
Yuki <protutorial.05@gmail.com>
2018-09-19 09:54:19 +0000

    otameshi

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -11,5 +11,10 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
     
+    
+    アイウエオ
+    
+    
   end
 end
+
